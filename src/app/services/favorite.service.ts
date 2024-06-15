f07d736613f5fef463e65ab609cdc802c7b29589
Guilherme S. de Azevedo <guilherme.asazevedo@gmail.com>
2024-06-15 02:18:36 -0300

    Implementação de formControl e validações de formulário

--- src/app/services/favorite.service.ts
+++ src/app/services/favorite.service.ts
@@ -1,13 +1,13 @@
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { Movie } from "../shared/movie.model";
+import { MovieModel } from "../shared/models/movie.model";
 import { BaseService } from "./base.service";
 
 @Injectable()
 export class FavoriteService extends BaseService {
     
-    addFavorites(movie : Movie) : Observable<any>{
+    addFavorites(movie : MovieModel) : Observable<any>{
         let endpoint = `${this.getApiUrl()}/api/v1/favorites`;
         return this._httpClient.post(endpoint, movie, this.httpOption);
     }
